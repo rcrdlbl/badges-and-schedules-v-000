ba8446f7cd0f4701a20a13f532230d368675b879
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-30 00:20:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  return badges
+end
